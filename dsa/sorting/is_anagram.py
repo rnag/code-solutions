0266c2dd5a4be7e51ae41584ec141ca16db701ad
Ritvik Nag <nagrim7@gmail.com>
2022-04-09 19:49:38 -0400

    add ianagram

--- dsa/sorting/is_anagram.py
+++ dsa/sorting/is_anagram.py
@@ -6,9 +6,37 @@ An example of an anagram: "cinema" <-> "iceman"
 
 """
 from collections import defaultdict
+from timeit import timeit
 
 
 def valid_anagram(word1, word2):
+    len1 = len(word1)
+    len2 = len(word2)
+
+    # if the words are different lengths, they can't be equal
+    if len1 != len2:
+        return False
+
+    # empty strings are equal, and we don't need to run anything else
+    if not len1 and not len2:
+        return True
+
+    freq = defaultdict(int)
+
+    for c in word1:
+        freq[c] += 1
+
+    for c in word2:
+        # if it can't lookup letter (char) or it's zero, then it's not an anagram
+        if not freq[c]:
+            return False
+
+        freq[c] -= 1
+
+    return True
+
+
+def valid_anagram_old(word1, word2):
     len1 = len(word1)
     len2 = len(word2)
 
@@ -41,3 +69,10 @@ if __name__ == '__main__':
     assert not valid_anagram('awesome', 'awesom')   # False
     assert valid_anagram('qwerty', 'qeywrt')        # True
     assert valid_anagram('cinema', 'iceman')        # True
+
+    # I time it here for n=10,000 so we can see the "old" approach is worse.
+    # In fact it has worse space complexity, and slightly worse time complexity.
+    #
+    n = 10_000
+    print('approach 1: ', timeit("valid_anagram('cinema', 'iceman')", number=n, globals=globals()))
+    print('approach 2: ',  timeit("valid_anagram_old('cinema', 'iceman')", number=n, globals=globals()))
