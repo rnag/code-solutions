55e8244f3630ee346f91d28d51231241820c5e62
Ritvik Nag <nagrim7@gmail.com>
2022-04-14 20:54:52 -0400

    checkin bubble

---
+++ dsa/search/bubble.py
@@ -0,0 +1,42 @@
+"""
+Bubble Sort algorithm implementation in Python
+"""
+
+
+# Bubble Sort solution
+#
+# Time complexity: O(N^2)
+def bubble_sort(arr):
+    for i in range(len(arr), 0, -1):
+        no_swaps = True
+        for j in range(i - 1):
+            # print(arr, arr[j], arr[j+1])
+            if arr[j] > arr[j + 1]:
+                no_swaps = False
+                # swap the elements
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        # print('ONE PASS COMPLETE')
+        if no_swaps:
+            break
+    return arr
+
+
+def main():
+    result = bubble_sort([8, 1, 2, 3, 4, 5, 6, 7])
+    assert result == [1, 2, 3, 4, 5, 6, 7, 8]
+
+    l = [3, 2, 1, 7, 1]
+    bubble_sort(l)
+    assert l == [1, 1, 2, 3, 7]
+
+    l = [37, 45, 29, 8, 12, 88, -3]
+    bubble_sort(l)
+    assert l == [-3, 8, 12, 29, 37, 45, 88]
+
+    l = [7, 6, 5, 4, 3, 2, 1]
+    bubble_sort(l)
+    assert l == [1, 2, 3, 4, 5, 6, 7]
+
+
+if __name__ == '__main__':
+    main()
