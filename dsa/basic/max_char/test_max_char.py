5f7609304d837e109c4da1d78ec3311f6492ae88
Ritvik Nag <nagrim7@gmail.com>
2022-04-20 19:30:14 -0400

    add max chr

--- dsa/basic/max_char/test_max_char.py
+++ dsa/basic/max_char/test_max_char.py
@@ -2,10 +2,10 @@ from basic.max_char.max_char import max_char
 
 
 def test_max_char():
-    assert max_char("abcccccccd") == "c"
+    assert max_char("abcccccccd") == 'c'
     assert max_char('') is None
     assert max_char('h') == 'h'
     assert max_char('hi') == 'h'
     assert max_char('hello') == 'l'
     assert max_char('hi there person!') == 'e'
-    assert max_char("apple 1231111") == "1"
+    assert max_char("apple 1231111") == '1'
