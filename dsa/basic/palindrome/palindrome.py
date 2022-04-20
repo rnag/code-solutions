5a57111f99022eb99b494b6da39832fc84d686ea
Ritvik Nag <rnag@collegeboard.org>
2022-04-20 17:46:52 -0400

    add pldindomre

--- dsa/basic/palindrome/palindrome.py
+++ dsa/basic/palindrome/palindrome.py
@@ -10,3 +10,17 @@ if the string is a palindrome.
 
 def palindrome(s: str) -> bool:
     return s == s[::-1]
+
+
+def palindrome_optimized(s: str) -> bool:
+    sl = len(s)
+    half = sl // 2
+
+    # use `for` loop to check if string is same on both sides
+    # this is actually more efficient - O(N/2) - because we only
+    # iterate over half the length of the string.
+    for i in range(half):
+        if s[i] != s[sl - 1 - i]:
+            return False
+
+    return True
--- dsa/basic/palindrome/test_palindrome.py
+++ dsa/basic/palindrome/test_palindrome.py
@@ -1,6 +1,13 @@
-from basic.palindrome.palindrome import palindrome
+from basic.palindrome.palindrome import palindrome, palindrome_optimized
 
 
 def test_palindrome():
     assert palindrome('abba')
+    assert palindrome('RACECAR')
     assert not palindrome('abcdefg')
+
+
+def test_palindrome_optimized():
+    assert palindrome_optimized('abba')
+    assert palindrome_optimized('RACECAR')
+    assert not palindrome_optimized('abcdefg')
