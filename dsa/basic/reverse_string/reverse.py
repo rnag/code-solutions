60e624a85052bb7bb8a424547040d0a5031de082
Ritvik Nag <rnag@collegeboard.org>
2022-04-20 17:11:35 -0400

    add str reverse

--- dsa/basic/reverse_string/reverse.py
+++ dsa/basic/reverse_string/reverse.py
@@ -3,10 +3,19 @@ Reverse a string in Python
 """
 
 
+def reverse_idiomatic(s: str) -> str:
+    return s[::-1]
+
+
 def reverse(s: str) -> str:
     reversed_chars = [s[i] for i in range(len(s) - 1, -1, -1)]
     return ''.join(reversed_chars)
 
 
-def reverse_idiomatic(s: str) -> str:
-    return s[::-1]
+def reverse_v2(s: str) -> str:
+    reversed_str = ''
+
+    for c in s:
+        reversed_str = c + reversed_str
+
+    return reversed_str
--- dsa/basic/reverse_string/test_reverse.py
+++ dsa/basic/reverse_string/test_reverse.py
@@ -1,4 +1,10 @@
-from basic.reverse_string.reverse import reverse, reverse_idiomatic
+from basic.reverse_string.reverse import reverse_idiomatic, reverse, reverse_v2
+
+
+def test_reverse_idiomatic():
+    assert reverse_idiomatic('apple') == 'elppa'
+    assert reverse_idiomatic('hello') == 'olleh'
+    assert reverse_idiomatic('Greetings!') == '!sgniteerG'
 
 
 def test_reverse():
@@ -7,7 +13,7 @@ def test_reverse():
     assert reverse('Greetings!') == '!sgniteerG'
 
 
-def test_reverse_idiomatic():
-    assert reverse_idiomatic('apple') == 'elppa'
-    assert reverse_idiomatic('hello') == 'olleh'
-    assert reverse_idiomatic('Greetings!') == '!sgniteerG'
+def test_reverse_v2():
+    assert reverse_v2('apple') == 'elppa'
+    assert reverse_v2('hello') == 'olleh'
+    assert reverse_v2('Greetings!') == '!sgniteerG'
